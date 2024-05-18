533cac298cafefc8bc3bceffdeabfdc41b3c660b
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-18 15:33:41 -0600

    Add Main component and add css styles to it

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import StartScreen from "./StartScreen";
+import "../styles/Main.css";
+
+export default function Main() {
+  return (
+    <main className="main">
+      <StartScreen />
+    </main>
+  );
+}
