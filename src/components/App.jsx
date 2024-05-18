ec4acf18efb78d85e59fd140c565dec6ff261fc0
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-18 15:33:54 -0600

    Add StartScreen component and add css styles to it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import "../styles/App.css";
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+import "../styles/StartScreen.css";
+
+export default function StartScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz</h2>
+      <h3>15 questions to test your React mastery</h3>
+      <button className="btn">Let's start</button>
+    </div>
+  );
+}
