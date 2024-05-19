c4a5de506498d5343a2c1d16707b0cacf45bc118
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-19 13:57:10 -0600

    Add useReducer hook with initial state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { useReducer } from "react";
 import "../styles/App.css";
 import Error from "./Error";
 import Header from "./Header";
 import Loader from "./Loader";
 import Main from "./Main";
 
+const initialState = {
+  status: "loading",
+};
+
+function reducer(state, action) {}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="app">
       <Header />
-      <Main />
-      <Loader />
-      <Error />
+
+      {state.status === "loading" && <Loader />}
+
+      {/* <Main />
+      <Error /> */}
     </div>
   );
 }
