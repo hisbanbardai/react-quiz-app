098254a2a7ba718926324f22a591e859eeed89de
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-21 15:51:50 -0600

    Dispatch action type startQuiz when user clicks on Let's Start button

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,13 @@
 import "../styles/StartScreen.css";
 
-export default function StartScreen() {
+export default function StartScreen({ dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz</h2>
       <h3>15 questions to test your React mastery</h3>
-      <button className="btn">Let's start</button>
+      <button onClick={() => dispatch({ type: "startQuiz" })} className="btn">
+        Let's start
+      </button>
     </div>
   );
 }
