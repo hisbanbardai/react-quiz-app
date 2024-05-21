9eeb9384987de3c04a47eb3785fe84b3d81c72d0
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-21 15:15:58 -0600

    Add useEffect to fetch data. Update state when data is received successfully or not

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,24 +4,52 @@ import Error from "./Error";
 import Header from "./Header";
 import Loader from "./Loader";
 import Main from "./Main";
+import { useEffect } from "react";
 
 const initialState = {
   status: "loading",
+  questions: [],
 };
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "received", questions: action.payLoad };
+
+    case "errorReceived":
+      return { ...state, status: "error" };
+  }
+}
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch("/api/questions");
+        console.log(res);
+
+        if (!res.ok) throw new Error("Unable to fetch data");
+
+        const data = await res.json();
+        console.log(data);
+        dispatch({ type: "dataReceived", payLoad: data });
+      } catch (error) {
+        console.error(error.message);
+        dispatch({ type: "errorReceived" });
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-
       {state.status === "loading" && <Loader />}
-
-      {/* <Main />
-      <Error /> */}
+      {state.status === "error" && <Error />}
+      {state.status === "received" && <Main />}
     </div>
   );
 }
