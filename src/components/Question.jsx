5e9478b43df59192b848b12dec2f97c902ce5983
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-21 15:58:16 -0600

    Add Options component in Question component and pass question's options as a prop

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,12 @@
+import Options from "./Options";
+
 export default function Question({ question }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <div className="options">Options</div>
+      <div className="options">
+        <Options options={question.options} />
+      </div>
     </div>
   );
 }
