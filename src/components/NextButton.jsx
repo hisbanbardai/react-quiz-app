00805df980e7d3804ea8c2d126edea342f91d895
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-24 13:26:18 -0600

    Add a NextButton component and add css styles to it

---
+++ src/components/NextButton.jsx
@@ -0,0 +1,9 @@
+import "../styles/NextButton.css";
+
+export default function NextButton() {
+  return (
+    <div className="next">
+      <button className="btn">Next</button>
+    </div>
+  );
+}
