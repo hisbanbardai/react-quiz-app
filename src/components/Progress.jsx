c8c803eb3eb00dfbe0ba4b7b956d8ad6615da43f
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-24 15:49:53 -0600

    Add css styles to Progress component

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,10 +1,16 @@
+import "../styles/Progress.css";
+
 export default function Progress() {
   return (
     <div className="progress-container">
       <progress max={"15"} value={"1"}></progress>
       <div className="progress-details">
-        <p>Question 1/15</p>
-        <p>0/280 points</p>
+        <p>
+          Question <b>1</b>/15
+        </p>
+        <p>
+          <b>0</b>/280 points
+        </p>
       </div>
     </div>
   );
