99281106fd9dc6b5da014acc4f559c249624df47
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-24 14:26:44 -0600

    Add useRef to scroll into view the next button

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useRef } from "react";
 import "../styles/NextButton.css";
 
 export default function NextButton({ dispatch, selectedAnswerIndex }) {
+  const nextButtonRef = useRef(null);
+
+  useEffect(
+    function () {
+      if (selectedAnswerIndex !== null && nextButtonRef.current !== null) {
+        nextButtonRef.current.scrollIntoView({ behavior: "smooth" });
+      }
+    },
+    [selectedAnswerIndex]
+  );
+
   return (
     selectedAnswerIndex !== null && (
       <div className="next">
         <button
           className="btn"
           onClick={() => dispatch({ type: "nextQuestion" })}
+          ref={nextButtonRef}
         >
           Next
         </button>
