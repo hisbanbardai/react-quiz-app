d94ece4399e1e014d96786192629e6272211d201
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-26 15:31:20 -0600

    Add a state variable timer using useState hook and update its state inside useEffect hook

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
 import "../styles/Timer.css";
 
 export default function Timer() {
-  return <span className="timer">10:00</span>;
+  const [timer, setTimer] = useState(10000);
+
+  useEffect(function () {
+    const id = setInterval(() => {
+      setTimer((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return <span className="timer">{timer}</span>;
 }
