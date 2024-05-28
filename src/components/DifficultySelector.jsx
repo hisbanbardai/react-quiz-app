a30e61c331939041bb675260e320076cbc834e06
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-28 07:39:06 -0600

    Add a DifficultySelector component with basic html structure

---
+++ src/components/DifficultySelector.jsx
@@ -0,0 +1,11 @@
+export default function DifficultySelector() {
+  return (
+    <select name="difficulty" className="difficulty">
+      <option value="">Select difficulty</option>
+      <option value="all">All</option>
+      <option value="easy">Easy</option>
+      <option value="medium">Medium</option>
+      <option value="hard">Hard</option>
+    </select>
+  );
+}
