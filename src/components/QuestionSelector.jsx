3cb311d95b88c3bcacfb700db39c2a18b050f837
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-29 12:56:06 -0600

    Receive all props from App component and update the input element by adding value and onChange attributes

--- src/components/QuestionSelector.jsx
+++ src/components/QuestionSelector.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import "../styles/QuestionSelector.css";
 
-export default function QuestionSelector({ difficulty }) {
+export default function QuestionSelector({
+  difficulty,
+  numOfQuestions,
+  selectedQuestions,
+  dispatch,
+}) {
+  function handleChange(e) {
+    console.log("g");
+    dispatch({
+      type: "updateNumOfQuestions",
+      payLoad: Number(e.target.value),
+    });
+  }
+
   return (
     difficulty && (
       <>
         <p className="questions-label">Select number of questions:</p>
-        <input type="range" max={5} className="questions-range" />
-        <p className="questions-total">Question(s): 5</p>
+        <input
+          className="questions-range"
+          type="range"
+          max={selectedQuestions.length}
+          value={numOfQuestions}
+          onChange={handleChange}
+        />
+        <p className="questions-total">Question(s): {numOfQuestions}</p>
       </>
     )
   );
