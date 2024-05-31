4b443121a6b5d7470d150393ce8a90383bd1f4e0
hisbanshiraz <hisbanbardai@gmail.com>
2024-05-31 11:30:44 -0600

    Remove unwanted code

--- src/components/PreviousButton.jsx
+++ src/components/PreviousButton.jsx
@@ -1,14 +1,11 @@
 import { useRef } from "react";
 
 export default function PreviousButton({ currentQuestionIndex, dispatch }) {
-  const previousButtonRef = useRef(null);
-
   return (
     currentQuestionIndex > 0 && (
       <button
         className="btn"
         onClick={() => dispatch({ type: "previousQuestion" })}
-        ref={previousButtonRef}
       >
         Previous
       </button>
